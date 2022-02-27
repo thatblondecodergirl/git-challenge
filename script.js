fb428311897be6241ea8896fb3d6e9ac9b1c54f1
thatblondecodergirl <madeleine.alexandersson@gmail.com>
2022-02-27 16:06:26 +0000

    Changed temperature conversion

--- script.js
+++ script.js
@@ -61,18 +61,6 @@ function getCurrentLocation(event) {
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 66;
-}
-
-function convertToCelsius(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 19;
-}
-
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
